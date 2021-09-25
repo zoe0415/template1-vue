c87dbaa4f0db15c18188cd58bc120cacefeb4be1
zoe <zoe@zoedeMacBook-Air.local>
2021-09-25 21:16:53 +0800

    install fontawesome

--- src/main.js
+++ src/main.js
@@ -3,6 +3,12 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import "tailwindcss/tailwind.css"
+// fontawesome
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+library.add(faBars)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false;
 
