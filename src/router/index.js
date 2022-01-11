27c22b808e27c4dd4ec90c563d56c08d9586ed56
zoe0415 <chenzoe15@gmail.com>
2022-01-11 23:37:53 +0800

    部署專案

--- src/router/index.js
+++ src/router/index.js
@@ -51,8 +51,10 @@ const routes = [
 ]
 
 const router = new VueRouter({
-  mode: "history",
-  base: process.env.BASE_URL,
+  // mode: "history",
+  // base: process.env.BASE_URL,
+  mode: "hash",
+  base: "/template1-vue",
   routes,
 })
 
